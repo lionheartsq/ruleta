598b16e2c0e70ec97d759341610617a6b55f92a7
Luis García <lujogarpin78@gmail.com>
2022-07-15 16:41:18 -0500

    Ajuste validaciones primarias

--- app/Http/Controllers/SalasController.php
+++ app/Http/Controllers/SalasController.php
@@ -12,6 +12,7 @@ class SalasController extends Controller
     public function crearsalas(Request $request)
     {
         //Debe crear la string random y añadir el id jugador como mod
+        // /salas/crear?capacidadSala=5&tipoJuego=1&nPuntaje=1000&nTurnos=10&urlRandom=prueba&idJugador=1
 
         // if(!$request->ajax()) return redirect('/');
         $randString=substr(str_shuffle('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'),0,10);
@@ -26,93 +27,96 @@ class SalasController extends Controller
         $Sala->estado=1;
         $Sala->save();
 
-        $respuesta="Ok sala";
-
         //posterior debe crear el registro en detalladosalas con los datos de la persona que creo (mod) como jugador
 
         $detalladosalas = new DetalladoSalas();
         $idSala = $Sala->id;
         $puntaje=0;
         $turno=0;
-      
+
         $detalladosalas->idJugador=$request->idJugador;
         $detalladosalas->idSala=$idSala;
         $detalladosalas->turno=$turno;
         $detalladosalas->puntaje=$puntaje;
-        $detalladosalas->save();  
+        $detalladosalas->save();
 
-        $respuesta="Ok detallado";
+        $respuesta="Ok";
 
-        return ['respuesta' => $respuesta];       
+        return ['respuesta' => $respuesta];
 
     }
 
     public function ingresarsalas(Request $request)
     {
-        $stringsala=$request->urlRandom;
+        $stringsala=$request->id;
+        $idJugador=$request->idJugador;
 
-        //con este busco en salas cual lo tiene como string y que esté en espera, de ahi me traigo el idsala 
-             
-        $capacidadSala=$request->capacidadSala; //??
-        $estado=1;
-        $idSala=$request->idSala; //??
-        
+        // /salas/ingresar?id=85UIQN0RKB&idJugador=1
+        //con este busco en salas cual lo tiene como string y que esté en espera, de ahi me traigo el idsala
 
-        if($stringsala == $request->urlRandom and $estado==1){
+        $respuesta = Salas::where('urlRandom', '=', $stringsala)
+        ->where('estado', '=', '1')
+        ->orderBy('id','asc')->get();
+
+
+        if($respuesta->isNotEmpty()){
+            foreach($respuesta as $guia){ //apertura foreach nomina
+                $idSala = $guia->id;
+                $capacidadSala = $guia->capacidadSala;
+                }
+            $flag=1;
+        }
+        else{
+            $flag=0;
+        }
+
+        if($flag==1){
             //mysql peticion
-            // $cantusuarios = select count(*) from detalladosalas ds where ds.idSala=$idSala; 
-            // $cantusuarios = Salas::join('salas','detalladosalas.idSala','=','salas.id')
-            $cantusuarios = DetalladoSalas::join('salas','detalladosalas.idSala','=','salas.id')
-            
-            ->selectRaw('count(*) as total')
-            ->groupBy('salas.id')
-            
-            
+            $cantusuarios = Salas::join('detalladosalas','salas.id','=','detalladosalas.idSala')
             ->where('detalladosalas.idSala','=', $idSala)
-            ->where('salas.estado','=',1)
-            
-            ->get();
+            ->count();
 
-            if($capacidadSala > $cantusuarios){
-                $detalladosalas = new DetalladoSalas();
+                if($capacidadSala > $cantusuarios){
+
+              $detalladosalas = new DetalladoSalas();
 
                 $puntaje=0;
                 $turno=0;
-              
-                $detalladosalas->idJugador=$request->idJugador;
+
+                $detalladosalas->idJugador=$idJugador;
                 $detalladosalas->idSala=$idSala;
                 $detalladosalas->turno=$turno;
                 $detalladosalas->puntaje=$puntaje;
-                $detalladosalas->save();  
+                $detalladosalas->save();
 
-                  
-        
-                $respuesta="Ok detallado";
+                if($capacidadSala==($cantusuarios-1)){
+                    //cambio estado de la sala y salgo
+                }
+                else{
+                    //solo salgo
+                }
+
+                $respuesta="Usuario ingresa con éxito";
             }
             else {
+                //
                 $respuesta="Pailas se lleno la sala";
-                
-
             }
         }
         else{
             $respuesta="Pailas la sala no esta disponible";
-           
-
         }
 
         //validaciones: si excede los usuarios hay tabla. si la sala esta activa hay tabla. si la sala esta cerrada madrugue.
 
-
-
-        return ['respuesta' => $respuesta];       
+        return ['respuesta' => $respuesta]; /**/
 
     }
 
     public function editarsalas(Request $request)
     {
         //Debe validar el cambio de estado
-        
+
 
         if(!$request->ajax()) return redirect('/');
         $Salas = Salas::findOrFail($request->id);
@@ -121,7 +125,7 @@ class SalasController extends Controller
     }
 
     public function validarsalas(Request $request){
-/* 
+/*
         //aca verifica que la sala este cumpliendo con el parametro, si llega al tope se cierra
         $capacidad= Salas::findOrFail($request->id)->get();
 
@@ -129,14 +133,14 @@ class SalasController extends Controller
             $Salas->estado=1;
         }
         if($capacidad==capacidadSala.value){
-            $Salas->estado=2; 
+            $Salas->estado=2;
         }
         if($capacidad==capacidadSala.value && nTurnos.value()==turnos.value() && nPuntaje.value()==puntos.value()){
             $Salas->estado=3;
         }
         $Salas= new Salas();
         $Salas->capacidadSala=$request->capacidadSala;
-        
+
         //$respuesta = Salas::orderBy('id','asc')->get();
         $respuesta = "blablabla";
 
@@ -148,8 +152,8 @@ class SalasController extends Controller
         //desde aca puede validar el estado de los turnos (historico)
 
             $respuesta = Salas::orderBy('id','asc')->get();
-    
+
             return ['respuesta' => $respuesta];
-        }        
+        }
 
 }
